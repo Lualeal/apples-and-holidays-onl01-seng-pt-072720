5c56aa2d31266b9c602616e9dd327d2ad8207cd3
Luana Leal <luana.leal.eq@gmail.com>
2020-08-06 19:07:38 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -62,12 +62,15 @@ end
 
 
 
-
-
 def all_holidays_with_bbq(holiday_hash)
-  # return an array of holiday names (as symbols) where supply lists
-  # include the string "BBQ"
-
+  holyday_hash.each do |season, holiday|
+  holiday
+  holiday.each do |value|
+    if value == "BBQ"
+      holiday
+    end
+    end 
+  end 
 end
 
 
