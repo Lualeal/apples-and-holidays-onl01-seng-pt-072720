b9ca56831419d1dcb81bb2b10666d19a6bdff8f8
Luana Leal <luana.leal.eq@gmail.com>
2020-08-06 18:58:05 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -55,12 +55,19 @@ end
 
 
 def all_supplies_in_holidays(holiday_hash)
-  holyday_hash.each do |season, holyday, supply|
+  holyday_hash.each do |season, holyday|
     
 
 end
 
-
+def all_supplies_in_holidays(holiday_hash)
+  holiday_hash.each do |season, holiday|
+    puts "#{season.capitalize}:"
+    holiday.each do |holiday, supplies|
+    puts "  #{holiday.to_s.split("_").map{|x| x.capitalize}.join(" ")}: #{supplies.join(", ")}"
+  end
+end
+end
 
 
 
